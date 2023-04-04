615c4d76b939d955eb82064bb46a883542fa390c
Jared M Kodero <jm.kodero@gmail.com>
2023-04-03 22:39:07 -0400

    updated paths

--- biogeography_module/module/paths.py
+++ biogeography_module/module/paths.py
@@ -1,30 +1,37 @@
 import dependencies 
 
-# File paths for various input and output files
-moisture_stress_path = dependencies.os.path.join("..", "data", "MMDI.csv") # Path to the file containing moisture stress data for a location and period.
-mean_moisture_stress_path = dependencies.os.path.join("..", "data", "MDI.csv") # Path to the file containing mean moisture stress data for a location and period.
-climate_path = dependencies.os.path.join("..", "data", "temp-gdd-prec.csv") # Path to the file containing climate data (temperature, precipitation, and growing degree days) for a location and time period.
-climate_limits_path = dependencies.os.path.join("..", "data", "bio_limit.csv") # Path to the file containing limits on the climate variables required for vegetation to grow.
+THIS_FOLDER = dependencies.Path(__file__).parent.resolve()
+dependencies.os.chdir(THIS_FOLDER)
 
-# File paths for various output variables
+
+# Define the file paths using pathlib, which works across different operating systems
+# Input file paths
+moisture_stress_path = THIS_FOLDER / ".." / "data" / "MMDI.csv"  # Path to moisture stress data
+mean_moisture_stress_path = THIS_FOLDER / ".." / "data" / "MDI.csv"  # Path to mean moisture stress data
+climate_path = THIS_FOLDER / ".." / "data" / "temp-gdd-prec.csv"  # Path to climate data (temperature, precipitation, growing degree days)
+climate_limits_path = THIS_FOLDER / ".." / "data" / "bio_limit.csv"  # Path to limits on climate variables required for vegetation growth
+
+# Output file paths
 tem_output_paths = {
-    "npp_path": dependencies.os.path.join("..", "data", "NPP.csv"), # Path to the file containing net primary productivity data.
-    "vegc_path": dependencies.os.path.join("..", "data", "VEGC.csv"), # Path to the file containing vegetation carbon data.
-    "nep_path": dependencies.os.path.join("..", "data", "NEP.csv"), # Path to the file containing net ecosystem productivity data.
-    "availn_path": dependencies.os.path.join("..", "data", "AVAILN.csv"), # Path to the file containing available nitrogen data.
-    "gpp_path": dependencies.os.path.join("..", "data", "GPP.csv"), # Path to the file containing gross primary productivity data.
-    "h2oyield_path": dependencies.os.path.join("..", "data", "H2OYIELD.csv"), # Path to the file containing water yield data.
-    "netnmin_path": dependencies.os.path.join("..", "data", "NETNMIN.csv"), # Path to the file containing net nitrogen mineralization data.
-    "smois_path": dependencies.os.path.join("..", "data", "SMOIS.csv"), # Path to the file containing soil moisture data.
-    "soilorgc_path": dependencies.os.path.join("..", "data", "SOILORGC.csv"), # Path to the file containing soil organic carbon data.
-    "veginnpp_path": dependencies.os.path.join("..", "data", "VEGINNPP.csv"), # Path to the file containing vegetation input NPP data.
-    "nce_path": dependencies.os.path.join("..", "data", "NCE.csv") # Path to the file containing net carbon exchange data.
+    "npp_path": THIS_FOLDER / ".." / "data" / "NPP.csv",  # Path to net primary productivity data
+    "vegc_path": THIS_FOLDER / ".." / "data" / "VEGC.csv",  # Path to vegetation carbon data
+    "nep_path": THIS_FOLDER / ".." / "data" / "NEP.csv",  # Path to net ecosystem productivity data
+    "availn_path": THIS_FOLDER / ".." / "data" / "AVAILN.csv",  # Path to available nitrogen data
+    "gpp_path": THIS_FOLDER / ".." / "data" / "GPP.csv",  # Path to gross primary productivity data
+    "h2oyield_path": THIS_FOLDER / ".." / "data" / "H2OYIELD.csv",  # Path to water yield data
+    "netnmin_path": THIS_FOLDER / ".." / "data" / "NETNMIN.csv",  # Path to net nitrogen mineralization data
+    "smois_path": THIS_FOLDER / ".." / "data" / "SMOIS.csv",  # Path to soil moisture data
+    "soilorgc_path": THIS_FOLDER / ".." / "data" / "SOILORGC.csv",  # Path to soil organic carbon data
+    "veginnpp_path": THIS_FOLDER / ".." / "data" / "VEGINNPP.csv",  # Path to vegetation input NPP data
+    "nce_path": THIS_FOLDER / ".." / "data" / "NCE.csv"  # Path to net carbon exchange data
 }
 
-# File paths for model bakeoff results
-bakeoff_results_path = dependencies.os.path.join("..", "data", "output_bakeoff", "npp_bakeoff_result.csv") # Path to the file containing the results of a model bakeoff for NPP.
+# Model bakeoff results file path
+bakeoff_results_path = THIS_FOLDER / ".." / "data" / "output_bakeoff" / "npp_bakeoff_result.csv"
 
-# File paths for model output for different time periods
-early_century_persisting_pft_output_path = dependencies.os.path.join("..", "data", "output_bakeoff", "2015-2045_bakeoff.csv") # Path to the file containing output data for the model simulation for the period 2015-2045, assuming that the plant functional types (PFTs) persist throughout the simulation.
-mid_century_persisting_pft_output_path = dependencies.os.path.join("..", "data", "output_bakeoff", "2045-2070_bakeoff.csv") # Path to the file containing output data for the model simulation for the period 2045-2070, assuming that the PFTs persist throughout the simulation.
-end_century_persisting_pft_output_path = dependencies.os.path.join("..", "data", "output_bakeoff", "2070-100_bakeoff.csv") # Path to the file containing output data for the model simulation for the period 2070-2100, assuming that the PFTs persist throughout the simulation.
+# Model output file paths for different time periods
+early_century_persisting_pft_output_path = THIS_FOLDER / ".." / "data" / "output_bakeoff" / "2015-2045_bakeoff.csv"  # Path to output data for 2015-2045, assuming  PFTs persist
+mid_century_persisting_pft_output_path = THIS_FOLDER / ".." / "data" / "output_bakeoff" / "2045-2070_bakeoff.csv"  # Path to output data for 2045-2070, assuming PFTs persist
+end_century_persisting_pft_output_path = THIS_FOLDER / ".." / "data" / "output_bakeoff" / "2070-100_bakeoff.csv" # Path to output data for 2070-2100, assuming PFTs persist
+
+
