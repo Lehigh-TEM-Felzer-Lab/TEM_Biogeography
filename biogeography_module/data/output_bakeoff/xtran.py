25bcad14cb4f1ecfd95e50d886a2d08044a6783f
Jared M Kodero <jm.kodero@gmail.com>
2023-04-09 17:24:06 -0400

    xtran

--- biogeography_module/data/output_bakeoff/xtran.py
+++ biogeography_module/data/output_bakeoff/xtran.py
@@ -12,7 +12,10 @@ import re
 import sys
 
 
+
+
 def process_file(input_filename,filter_params):
+    
     var_cols = [
         "LON",
         "LAT",
@@ -47,48 +50,114 @@ def process_file(input_filename,filter_params):
         "REGION",
     ]
     df = pd.read_csv(input_filename,names=var_cols)
+    df["VARIABLE"] = df["VARIABLE"].str.strip()
     df["REGION"] = df["REGION"].str.strip()
+    df["LAT"]=df["LAT"].round(1)
+    df["LON"]=df["LON"].round(1)
     
-    # Function to filter the dataframe based on the filter parameters
+    
+    variable_parts = df["VARIABLE"].unique()
+    variable_first_part = variable_parts[0]
+    stats_out_file = variable_first_part + ".SUMMARY"
+    units_out_file = variable_first_part + ".UNITS"
+    
+    # Define the function to get units
+    def get_units(variable, column):
+        value = df["VARIABLE"][0]
+        if value == "CH4FLUX" or value == "CH4EMISS" or value == "CH4CNSMP":
+            if column == "MNBYAR":
+                return "mgC/m^2"
+            elif column == "TOTFORECOZONE":
+                return "(gX10^12)"
+        elif value == "VSTRUCTN" or value == "SOILORGN" or value == "VEGN":
+            if column == "MNBYAR":
+                return "gN/m^2"
+            elif column == "TOTFORECOZONE":
+                return "Tg"
+        elif value in [
+            "VSTOREN",
+            "AVAILN",
+            "NETNMIN",
+            "NLOST",
+            "NINPUT",
+            "VEGNUP",
+            "LTRN",
+            "MICRONUP",
+            "VNMOBIL",
+            "VNRESORB",
+            "VEGSUP",
+            "VEGLUP",
+            "N2OFLUX",
+        ]:
+            if column == "MNBYAR":
+                return "mgN/m^2"
+            elif column == "TOTFORECOZONE":
+                return "(gX10^9)"
+        else:
+            if column == "MNBYAR":
+                return "gC/m^2"
+            elif column == "TOTFORECOZONE":
+                return "(gX10^12)"
+        if column == "TOTCELLAREA":
+            return "m^2"
+        else:
+            return ""
+
+    # Define the columns to get units for
+    columns = [
+        "TOTCELLAREA",
+        "TOTFORECOZONE",
+        "MNBYAR"
+    ]
+
+    # Open the output file and write the units for each variable and column
+    with open(units_out_file, "w") as f:
+        variables = df["VARIABLE"].unique()
+        for variable in variables:
+            for column in columns:
+                units = get_units(variable, column)
+                f.write(f"{variable}-{column}: {units}\n")
+    
     def filter_dataframe(df, filter_params):
         if filter_params is not None:
-            filter_criteria = {}
+            filter_criteria = []
             if filter_params.get("lat_min") is not None:
                 try:
-                    filter_criteria["LAT >= "] = float(filter_params["lat_min"])
+                    lat_min = float(filter_params["lat_min"])
+                    filter_criteria.append(f"LAT >= {lat_min}")
                 except ValueError:
                     print("Invalid value for lat_min. Must be a float.")
             if filter_params.get("lat_max") is not None:
                 try:
-                    filter_criteria["LAT <= "] = float(filter_params["lat_max"])
+                    lat_max = float(filter_params["lat_max"])
+                    filter_criteria.append(f"LAT <= {lat_max}")
                 except ValueError:
                     print("Invalid value for lat_max. Must be a float.")
             if filter_params.get("lon_min") is not None:
                 try:
-                    filter_criteria["LON >= "] = float(filter_params["lon_min"])
+                    lon_min = float(filter_params["lon_min"])
+                    filter_criteria.append(f"LON >= {lon_min}")
                 except ValueError:
                     print("Invalid value for lon_min. Must be a float.")
             if filter_params.get("lon_max") is not None:
                 try:
-                    filter_criteria["LON <= "] = float(filter_params["lon_max"])
+                    lon_max = float(filter_params["lon_max"])
+                    filter_criteria.append(f"LON <= {lon_max}")
                 except ValueError:
                     print("Invalid value for lon_max. Must be a float.")
             if filter_params.get("region") is not None:
-                try:
-                    filter_criteria["REGION == "] = f"'{filter_params['region']}'"
-                except KeyError:
-                    print("region parameter missing from filter_params.")
-            
+                region = filter_params.get("region")
+                filter_criteria.append(f"REGION == '{region}'")
+
             if filter_criteria:
-                query_string = " and ".join([k + str(v) for k, v in filter_criteria.items()])
+                query_string = " and ".join(filter_criteria)
                 try:
                     df = df.query(query_string)
                 except ValueError:
-                    print("Invalid filter criteria. Please check your parameters in the XML file and try again.")
-                except KeyError:
-                    print("Invalid filter criteria. Please check your parameters in the XML file and try again.")
+                    print("Invalid filter criteria. Please check your parameters and try again.")
 
         return df
+
 
     df = filter_dataframe(df, filter_params)
 
@@ -137,11 +206,6 @@ def process_file(input_filename,filter_params):
     # Concatenate the two summary statistics dataframes
     summary_stats_final = pd.concat([summary_stats_potveg, pd.DataFrame(np.nan, index=[0], columns=summary_stats_potveg.columns), summary_stats_region])
     summary_stats_final = summary_stats_final.round(2)
-
-    variable_parts = df["VARIABLE"].unique()[0].split()
-    variable_first_part = variable_parts[0]
-    stats_out_file = variable_first_part + ".SUMMARY"
-    units_out_file = variable_first_part + ".UNITS"
 
     summary_stats_final=summary_stats_final.drop("REGION",axis=1)
     
@@ -208,66 +272,7 @@ def process_file(input_filename,filter_params):
     # Save summary statistics to a CSV file
     summary_stats_final.to_csv(stats_out_file, index=False)
 
-    # Define the function to get units
-    def get_units(variable, column):
-        value = df["VARIABLE"][0]
-        if value == "CH4FLUX" or value == "CH4EMISS" or value == "CH4CNSMP":
-            if column == "MNBYAR":
-                return "mgC/m^2"
-            elif column == "TOTFORECOZONE":
-                return "(gX10^12)"
-        elif value == "VSTRUCTN" or value == "SOILORGN" or value == "VEGN":
-            if column == "MNBYAR":
-                return "gN/m^2"
-            elif column == "TOTFORECOZONE":
-                return "Tg"
-        elif value in [
-            "VSTOREN",
-            "AVAILN",
-            "NETNMIN",
-            "NLOST",
-            "NINPUT",
-            "VEGNUP",
-            "LTRN",
-            "MICRONUP",
-            "VNMOBIL",
-            "VNRESORB",
-            "VEGSUP",
-            "VEGLUP",
-            "N2OFLUX",
-        ]:
-            if column == "MNBYAR":
-                return "mgN/m^2"
-            elif column == "TOTFORECOZONE":
-                return "(gX10^9)"
-        else:
-            if column == "MNBYAR":
-                return "gC/m^2"
-            elif column == "TOTFORECOZONE":
-                return "(gX10^12)"
-        if column == "TOTCELLAREA":
-            return "m^2"
-        else:
-            return ""
-
-    # Define the columns to get units for
-    columns = [
-        "TOTCELLAREA",
-        "TOTFORECOZONE",
-        "MNBYAR"
-    ]
-
-    # Open the output file and write the units for each variable and column
-    with open(units_out_file, "w") as f:
-        variables = df["VARIABLE"].unique()
-        for variable in variables:
-            for column in columns:
-                units = get_units(variable, column)
-                f.write(f"{variable}-{column}: {units}\n")
-
-
-
-
+  
 def get_file_list(input_path):
     if input_path.endswith(".xml"):
         tree = ET.parse(input_path)
@@ -283,10 +288,10 @@ def get_file_list(input_path):
             lon_max = filter_params_elem.findtext("lon_max")
             region = filter_params_elem.findtext("region")
             if lat_min.strip() or lat_max.strip() or lon_min.strip() or lon_max.strip() or region.strip():
-                filter_params["lat_min"] = float(lat_min) if lat_min.strip() else None
-                filter_params["lat_max"] = float(lat_max) if lat_max.strip() else None
-                filter_params["lon_min"] = float(lon_min) if lon_min.strip() else None
-                filter_params["lon_max"] = float(lon_max) if lon_max.strip() else None
+                filter_params["lat_min"] = round(float(lat_min), 1) if lat_min.strip() else None
+                filter_params["lat_max"] = round(float(lat_max), 1) if lat_max.strip() else None
+                filter_params["lon_min"] = round(float(lon_min), 1) if lon_min.strip() else None
+                filter_params["lon_max"] = round(float(lon_max), 1) if lon_max.strip() else None
                 filter_params["region"] = region.strip() if region.strip() else None
         else:
             file_list = [input_path]
@@ -296,6 +301,7 @@ def get_file_list(input_path):
         filter_params = None
     
     return file_list, filter_params
+
 
 
 
