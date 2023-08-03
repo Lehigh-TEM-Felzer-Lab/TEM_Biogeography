36373115e9e81ebc7ab214838ad7bab6a0190fe1
Jared-Kodero <jm.kodero@gmail.com>
2023-08-02 22:50:51 -0400

    updates

--- biogeography_module/module/functions.py
+++ biogeography_module/module/functions.py
@@ -1,5 +1,6 @@
-import dependencies
+import dependencies 
 from tem_output_data import var_cols
+
 
 
 # Function to merge bakeoff variables
@@ -9,17 +10,13 @@ def merge_variables(df, climate_limits, climate, moisture_stress):
     # Merge the dataframe with climate on col, row and year columns
     df = df.merge(climate, on=["LON", "LAT", "YEAR"], how="left")
     # Merge the dataframe with moisture_stress on col, row, potveg, subtype and year columns
-    df = df.merge(
-        moisture_stress, on=["LON", "LAT", "POTVEG", "SUBTYPE", "YEAR"], how="left"
-    )
-
+    df = df.merge(moisture_stress, on=["LON", "LAT", "POTVEG", "SUBTYPE", "YEAR"], how="left")
+    
     return df
 
 
-# Function to calculate possible PFTs  in each gridcell depending on climate
+# Function to calculate possible PFTs  in each gridcell depending on climate 
 possible_cols = var_cols + ["NPP", "POSSIBLE"]
-
-
 def determine_possible_pft(df, bakeoff_variables):
     df["POSSIBLE"] = (
         (bakeoff_variables["GDD"] >= bakeoff_variables["MIN_GDD"])
@@ -28,16 +25,16 @@ def determine_possible_pft(df, bakeoff_variables):
         & (bakeoff_variables["T_MAX"] <= bakeoff_variables["Tw"])
         & (bakeoff_variables["AET/PET"] >= bakeoff_variables["MIN_AET/PET"])
     )
-
+    
     return df[possible_cols]
 
 
-# This code defines a function called "possible_pft_with_max_npp" that takes a group as an input.
-# The group is assumed to be a subset of a larger dataset that represents a grid cell with multiple plant functional types (PFTs) and their corresponding net primary productivity (NPP) values.
-# The function first checks if there are any PFTs in the group that are marked as "possible" (i.e., capable of growing in the current environmental conditions).
-# If there are, it selects the PFT with the highest NPP value among those that are marked as possible.
-# If there are no possible PFTs in the group, it selects the PFT with the highest NPP value among all PFTs in the group.
 
+#This code defines a function called "possible_pft_with_max_npp" that takes a group as an input.
+#The group is assumed to be a subset of a larger dataset that represents a grid cell with multiple plant functional types (PFTs) and their corresponding net primary productivity (NPP) values.
+#The function first checks if there are any PFTs in the group that are marked as "possible" (i.e., capable of growing in the current environmental conditions). 
+#If there are, it selects the PFT with the highest NPP value among those that are marked as possible. 
+#If there are no possible PFTs in the group, it selects the PFT with the highest NPP value among all PFTs in the group.
 
 def possible_pft_with_max_npp(group):
     # select max possible cohort in a group
@@ -52,34 +49,34 @@ def possible_pft_with_max_npp(group):
 
 # Function to clean dataframe
 def clean_dataframe(df):
-    df = df.drop(["POSSIBLE", "NPP"], axis=1)
+    
+    df = df.drop(["POSSIBLE","NPP"], axis=1)
 
     df.loc[df["ICOHORT"] > 0, "ICOHORT"] = 1
     df["SUBAREA"] = df["CAREA"]
     return df
 
 
+
 # Function to export dataframe to csv
 def export_to_csv(df, file_name):
     df.to_csv(file_name, index=False, header=False)
 
-
-# Function to Count which PFT have the max Total NPP (30 Year Period) after applying  Bioclimatic Limits and the NPP bakeoff
+    
+    
+ # Function to Count which PFT have the max Total NPP (30 Year Period) after applying  Bioclimatic Limits and the NPP bakeoff
 def persisting_pft(df, path):
     # Count the number of times each PFT has the max Total NPP (30 Year Period) after applying Bioclimatic Limits and the NPP bakeoff
-    count_max_npp = (
-        df.groupby(["LON", "LAT", "POTVEG"]).size().reset_index(name="COUNT")
-    )
-
+    count_max_npp = df.groupby(["LON", "LAT", "POTVEG"]).size().reset_index(name="COUNT")
+  
     # Find the PFT with the max NPP in each grid cell
-    idx = (
-        count_max_npp.groupby(["LON", "LAT"])["COUNT"].transform(max)
-        == count_max_npp["COUNT"]
-    )
+    idx = count_max_npp.groupby(["LON", "LAT"])["COUNT"].transform(max) == count_max_npp["COUNT"]
     persisting_pft = count_max_npp[idx]
 
+ 
     # Delete the unwanted 'count' column
     del persisting_pft["COUNT"]
 
     persisting_pft.to_csv(path, index=False, header=False)
     return persisting_pft
+
