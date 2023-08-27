6caf45c3b2e80c6f719117e33b5a61249e39fc6e
Jared-Kodero <jm.kodero@gmail.com>
2023-08-27 12:51:15 -0400

    update to run_tem.sh

--- run_tem.sh
+++ run_tem.sh
@@ -2,103 +2,137 @@
 
 set -euo pipefail
 
-echo "<<<Starting Biogeography Model >>>"
+# Function to display progress bar
+function progress_bar() {
+    local width=50
+    local percentage=$1
+    local completed=$((percentage * width / 100))
+    local remaining=$((width - completed))
+    echo " "
+
+    printf " Progress : [%s%s] %.2f%%\r" "$(yes "#" | head -n "$completed" | tr -d '\n')" "$(yes "-" | head -n "$remaining" | tr -d '\n')" "$percentage"
+    echo " "
+}
+
+echo " 0 1 2 3 4 5 6 7 8 9 "
+echo " "
+echo "<<< Starting Biogeography Model >>>"
+echo " "
 cd .
+
 # Directories
 tem_biogeography="."
 tem_core="$tem_biogeography/tem_core"
 runs="$tem_biogeography/runs"
 
-
-
-
-# Functions
+# Clean TEM core
 clean_tem_core() {
-  cd "$tem_core"
-  echo "Removing all .o files..."
-  rm -f *.o
-  wait  
-  echo "Done!"
+    cd "$tem_core"
+    echo "Removing all .o files"
+    rm -f *.o
+    wait
+   progress_bar 3
+    
 }
 
 clean_tem_core
 
-
+# Compile TEM
 compile_tem() {
-  echo "Compiling tem..."
-  make -f Makefile_biogeo.xtem xtem45_biogeo
-  wait
-  echo "Done!"
+    echo " "
+    echo "Compiling TEM"
+    echo " "
+
+    make -f Makefile_biogeo.xtem xtem45_biogeo
+    wait
+   
+    progress_bar 5
 }
 
 compile_tem
 
+# Copy executable
 copy_executable() {
-  echo "Copying tem executable to run directory..."
-  cp xtem45_biogeo "../runs"
-  wait
-  cd "../"
-  echo "Done!"
+    echo " "
+    echo "Copying tem executable to run directory."
+    cp xtem45_biogeo "../runs"
+    wait
+    cd "../"
+   
+    progress_bar 6
 }
 
 copy_executable
 
+# Remove TEMOUT files
 remove_temout_files() {
-  
-  echo "Removing all .TEMOUT files from directory..."
-  rm -f "$runs"/*.TEMOUT
-  wait
-  echo "Done!"
+    echo " "
+    echo "Removing all .TEMOUT files from runs dir..."
+    rm -f "$runs"/*.TEMOUT
+    wait
+   
+    progress_bar 7
 }
 
 cd "$runs"
 remove_temout_files "$runs"
 
+
+# Remove unnecessary files
 remove_unnecessary_files() {
-  echo "Removing unnecessary files..."
-  rm -f "$runs"/FIRE.csv
-  rm -f "$runs"/FIRE_VARS.csv
-  rm -f "$runs"/MMDI.csv
-  rm -f "$runs"/*.log
-  echo "Done!"
-  
+    echo " "
+    echo "Removing unnecessary files..."
+    rm -f FIRE.csv
+    rm -f FIRE_VARS.csv
+    rm -f MMDI.csv
+    rm -f *.log
+    echo " "
+
+   
+    progress_bar 8
 }
 
 remove_unnecessary_files
 
+# Run TEM executable
 run_tem_executable() {
-  echo "Running TEM executable..."
-  ./xtem45_biogeo 
-  wait
-  echo "Done!"
+     echo " "
+    echo "Running TEM "
+    echo " "
+    ./xtem45_biogeo
+    wait
+   
+    progress_bar  60
 }
-
 
 run_tem_executable
 wait
-echo "TEM run complete"
 
-biogeography_and_post_processing(){
-	
-  pwd
-  
 
-  echo "Running biogeography model..."
-  python main.py
-  wait 
-  echo "Done!"
-  echo "Creating Vegetation Maps.."
-  python vegetation_maps.py 
-  wait 	
-  echo "Done!"
-  echo "Running xtran .."
-  python xtran.py <<< xbatch.xml
-  wait
-  echo "Done!"
-  
+# Biogeography and post-processing
+biogeography_and_post_processing() {
+    pwd
+    echo " "
+    echo "Running biogeography model..."
+    python main.py &
+    wait
+   
+    progress_bar 80
+     echo " "
+    echo "Creating Vegetation Maps..."
+    python vegetation_maps.py &
+    wait
+   
+    progress_bar 90
+     echo " "
+    echo "Running xtran..."
+    python xtran.py < xbatch.xml
+    wait
+   
+    progress_bar  95
 }
 
 biogeography_and_post_processing
-
+ echo " "
+progress_bar  100
 echo "Complete!"
-
