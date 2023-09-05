f313aff860cce0703323e180e3fd281aa88e0504
Jared M Kodero <47959376+Jared-Kodero@users.noreply.github.com>
2023-09-05 12:01:13 -0400

    updated PowerShell script

--- run_tem.ps1
+++ run_tem.ps1
@@ -1,8 +1,8 @@
 # Define colors
-$CYAN = "`e[0;36m"
-$GREEN = "`e[0;32m"
-$YELLOW = "`e[0;33m"
-$NC = "`e[0m" # No Color
+$CYAN = "Cyan"
+$GREEN = "Green"
+$YELLOW = "Yellow"
+$NC = "White" # Reset Color
 
 # Function to display progress bar
 function progress_bar {
@@ -11,14 +11,14 @@ function progress_bar {
     $width = 50
     $completed = [Math]::Round(($percentage * $width) / 100)
     $remaining = $width - $completed
-    Write-Host "${YELLOW}Progress : [$('-' * $completed + '#' * $remaining)] $percentage%`r${NC}"
+    Write-Host "Progress : [$('#' * $completed + '-' * $remaining)] $percentage%`r" -ForegroundColor $YELLOW
 }
 
 # Header
-Write-Host ""
-Write-Host "${GREEN}*************************************************${NC}"
-Write-Host "${GREEN}***       Starting Biogeography Model        ***${NC}"
-Write-Host "${GREEN}*************************************************${NC}"
+Write-Host "" -ForegroundColor $NC
+Write-Host "*************************************************" -ForegroundColor $GREEN
+Write-Host "***       Starting Biogeography Model        ***" -ForegroundColor $GREEN
+Write-Host "*************************************************" -ForegroundColor $GREEN
 
 # Directories
 $tem_biogeography = "."
@@ -27,36 +27,36 @@ $runs = "$tem_biogeography/runs"
 
 function clean_tem_core {
     Set-Location $tem_core
-    Write-Host "${CYAN}*** Removing all .object files ***${NC}"
+    Write-Host "*** Removing all .object files ***" -ForegroundColor $CYAN
     Remove-Item *.o
     progress_bar 3
 }
 
 function compile_tem {
-    Write-Host ""
-    Write-Host "${CYAN}*** Compiling TEM ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Compiling TEM ***" -ForegroundColor $CYAN
     & make -f Makefile_biogeo.xtem xtem45_biogeo
     progress_bar 5
 }
 
 function copy_executable {
-    Write-Host ""
-    Write-Host "${CYAN}*** Copying TEM executable to run directory ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Copying TEM executable to run directory ***" -ForegroundColor $CYAN
     Copy-Item xtem45_biogeo "../runs"
     Set-Location "../"
     progress_bar 6
 }
 
 function remove_temout_files {
-    Write-Host ""
-    Write-Host "${CYAN}*** Removing all .temout files from runs dir ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Removing all .temout files from runs dir ***" -ForegroundColor $CYAN
     Remove-Item "$runs/*.TEMOUT"
     progress_bar 7
 }
 
 function prep_run_dir {
-    Write-Host ""
-    Write-Host "${CYAN}*** Preparing run directory ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Preparing run directory ***" -ForegroundColor $CYAN
     Remove-Item FIRE.csv, FIRE_VARS.csv, MMDI.csv, *.log
     Copy-Item ../biogeography_module/* .
     Copy-Item ../xtran/* .
@@ -64,8 +64,8 @@ function prep_run_dir {
 }
 
 function run_tem_executable {
-    Write-Host ""
-    Write-Host "${CYAN}*** Running TEM ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Running TEM ***" -ForegroundColor $CYAN
     Set-Location $runs
     Set-ExecutionPolicy Bypass -Scope Process -Force
     Start-Process ./xtem45_biogeo -Wait
@@ -73,18 +73,18 @@ function run_tem_executable {
 }
 
 function biogeography_and_post_processing {
-    Write-Host ""
-    Write-Host "${CYAN}*** Running biogeography model ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Running biogeography model ***" -ForegroundColor $CYAN
     & python main.py
     progress_bar 80
 
-    Write-Host ""
-    Write-Host "${CYAN}*** Running xtran ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Running xtran ***" -ForegroundColor $CYAN
     & python xtran.py xbatch.xml
     progress_bar 90
 
-    Write-Host ""
-    Write-Host "${CYAN}*** Creating Vegetation Maps ***${NC}"
+    Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Creating Vegetation Maps ***" -ForegroundColor $CYAN
     & python vegetation_maps.py
     progress_bar 95
 }
@@ -99,7 +99,7 @@ run_tem_executable
 biogeography_and_post_processing
 
 # Completion message
-Write-Host "${YELLOW}***${NC}"
+Write-Host "***" -ForegroundColor $YELLOW
 progress_bar 100
-Write-Host "${GREEN}*** Complete! ***${NC}"
-Write-Host "${YELLOW}*************************************************${NC}"
+Write-Host "*** Complete! ***" -ForegroundColor $GREEN
+Write-Host "*************************************************" -ForegroundColor $YELLOW
