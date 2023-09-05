1ccd7d185ab70b759a44af8363903e331796fbc8
Jared M Kodero <47959376+Jared-Kodero@users.noreply.github.com>
2023-09-05 12:36:55 -0400

    update to PowerShell script

--- run_tem.ps1
+++ run_tem.ps1
@@ -1,8 +1,13 @@
+# Set error handling
+$ErrorActionPreference = "Stop"
+
 # Define colors
 $CYAN = "Cyan"
 $GREEN = "Green"
 $YELLOW = "Yellow"
 $NC = "White" # Reset Color
+
+
 
 # Function to display progress bar
 function progress_bar {
@@ -20,85 +25,101 @@ Write-Host "*************************************************" -ForegroundColor 
 Write-Host "***       Starting Biogeography Model        ***" -ForegroundColor $GREEN
 Write-Host "*************************************************" -ForegroundColor $GREEN
 
-# Directories
-$tem_biogeography = "."
-$tem_core = "$tem_biogeography/tem_core"
-$runs = "$tem_biogeography/runs"
+# Set directories
+$tem_biogeography = Get-Location
+$tem_core = Join-Path $tem_biogeography "tem_core"
+$runs = Join-Path $tem_biogeography "runs"
 
-function clean_tem_core {
+# Function to clean tem_core
+function Clean-TemCore {
     Set-Location $tem_core
     Write-Host "*** Removing all .object files ***" -ForegroundColor $CYAN
-    Remove-Item *.o
+    Remove-Item -Path *.o -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path *.exe -Force -ErrorAction SilentlyContinue
     progress_bar 3
 }
 
-function compile_tem {
+Clean-TemCore
+
+# Function to compile tem
+function Compile-Tem {
     Write-Host "" -ForegroundColor $NC
     Write-Host "*** Compiling TEM ***" -ForegroundColor $CYAN
-    & make -f Makefile_biogeo.xtem xtem45_biogeo
+    Start-Process -Wait -FilePath "make" -ArgumentList "-f Makefile_biogeo.xtem xtem45_biogeo"
     progress_bar 5
+    
 }
 
-function copy_executable {
-    Write-Host "" -ForegroundColor $NC
+Compile-Tem
+
+# Function to copy executable
+function Copy-Executable {
+   Write-Host "" -ForegroundColor $NC
     Write-Host "*** Copying TEM executable to run directory ***" -ForegroundColor $CYAN
-    Copy-Item xtem45_biogeo "../runs"
-    Set-Location "../"
-    progress_bar 6
+    Copy-Item "xtem45_biogeo.exe" -Destination $runs -Force
+    Set-Location $runs
+     progress_bar 6
 }
 
-function remove_temout_files {
-    Write-Host "" -ForegroundColor $NC
+Copy-Executable
+
+# Function to remove .TEMOUT files
+function Remove-TemoutFiles {
+   Write-Host "" -ForegroundColor $NC
     Write-Host "*** Removing all .temout files from runs dir ***" -ForegroundColor $CYAN
-    Remove-Item "$runs/*.TEMOUT"
+    Remove-Item "$runs\*.TEMOUT" -Force -ErrorAction SilentlyContinue
     progress_bar 7
 }
 
-function prep_run_dir {
+Remove-TemoutFiles
+
+# Function to remove unnecessary files
+function Remove-UnnecessaryFiles {
     Write-Host "" -ForegroundColor $NC
     Write-Host "*** Preparing run directory ***" -ForegroundColor $CYAN
-    Remove-Item FIRE.csv, FIRE_VARS.csv, MMDI.csv, *.log
+    Remove-Item "$runs\FIRE.csv", "$runs\FIRE_VARS.csv", "$runs\MMDI.csv", "$runs\*.log" -Force -ErrorAction SilentlyContinue
     Copy-Item ../biogeography_module/* .
     Copy-Item ../xtran/* .
     progress_bar 8
+    
 }
 
-function run_tem_executable {
+Remove-UnnecessaryFiles
+
+# Function to run TEM executable
+function Run-TemExecutable {
     Write-Host "" -ForegroundColor $NC
     Write-Host "*** Running TEM ***" -ForegroundColor $CYAN
-    Set-Location $runs
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    Start-Process ./xtem45_biogeo -Wait
+    Start-Process -Wait -FilePath ".\xtem45_biogeo.exe"
     progress_bar 60
 }
 
-function biogeography_and_post_processing {
-    Write-Host "" -ForegroundColor $NC
-    Write-Host "*** Running biogeography model ***" -ForegroundColor $CYAN
-    & python main.py
-    progress_bar 80
+Run-TemExecutable
+
+
+
+# Function for biogeography and post-processing
+function Biogeography-And-PostProcessing {
+    $pwd = Get-Location
 
     Write-Host "" -ForegroundColor $NC
+    Write-Host "*** Running biogeography model ***" -ForegroundColor $CYAN
+    & python.exe main.py -Wait
+    
+    
+    Write-Host "" -ForegroundColor $NC
     Write-Host "*** Running xtran ***" -ForegroundColor $CYAN
-    & python xtran.py xbatch.xml
-    progress_bar 90
+    & python.exe xtran.py xbatch.xml -Wait
 
     Write-Host "" -ForegroundColor $NC
     Write-Host "*** Creating Vegetation Maps ***" -ForegroundColor $CYAN
-    & python vegetation_maps.py
-    progress_bar 95
+    & python.exe vegetation_maps.py -Wait
+    
+    progress_bar 90 
 }
 
-# Function calls
-clean_tem_core
-compile_tem
-copy_executable
-remove_temout_files
-prep_run_dir
-run_tem_executable
-biogeography_and_post_processing
+Biogeography-And-PostProcessing
 
-# Completion message
 Write-Host "***" -ForegroundColor $YELLOW
 progress_bar 100
 Write-Host "*** Complete! ***" -ForegroundColor $GREEN
