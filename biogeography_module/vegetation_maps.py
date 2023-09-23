9cc853e6a679dde12213e784812ed37bfab3436f
Jared M Kodero <47959376+Jared-Kodero@users.noreply.github.com>
2023-09-23 12:59:54 -0400

    Update vegetation_maps.py

--- biogeography_module/vegetation_maps.py
+++ biogeography_module/vegetation_maps.py
@@ -1,36 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-# Importing libraries and setting up the environment
-import warnings
-
-warnings.filterwarnings("ignore")  # setting ignore as a parameter
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import scipy.stats as stats
 import matplotlib.pyplot as plt
-import matplotlib.ticker as plticker
-from mpl_toolkits.basemap import Basemap
-from statsmodels.genmod.families import Binomial
-from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 from matplotlib.patches import Patch
-from scipy.interpolate import Rbf
-from scipy.interpolate import griddata
-image_format = "png"
+from matplotlib.colors import ListedColormap, BoundaryNorm
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import seaborn as sns
 
 
-
-# Historical Land Cover Color Map
-current_vegetation_cmap = ListedColormap(
-     ["#0F52BA", "#9ACD32", "#1b9d77", "#7570b3", "#A4D4B4", "#004953"]
+# Import Current Landcover Data
+historical_model = pd.read_csv("./biogeo/input/current_land_cover.csv",
+    names=["LON", "LAT", "TYPE"],
 )
 
-# Set Future Land Cover Color Map
-# Set Future Land Cover Color Map
+
+future_model = pd.read_csv("./biogeo/output_bakeoff/end.shift",
+    names=["LON", "LAT", "TYPE"],
+)
+
+
+# Future vegetation colormap and list
 future_vegetation_cmap = ListedColormap(
     [
         "#0F52BA",
@@ -44,15 +33,6 @@ future_vegetation_cmap = ListedColormap(
     ]
 )
 
-# Create a list of labels for the different colors in the colormap
-current_vegetation_list = [
-    "Boreal Forests",
-    "Mixed Temperate Forests",
-    "Temperate Coniferous Forests",
-    "Temperate Deciduous Forests",
-    "Xeromorphic Forests and Woodlands",
-    "Temperate Broadleaved Evergreen Forests",
-]
 future_vegetation_list = [
     "Boreal Forests",
     "Mixed Temperate Forests",
@@ -64,113 +44,71 @@ future_vegetation_list = [
     "Temperate Broadleaved Evergreen Forests",
 ]
 
+def create_patches(cmap, vegetation_list):
+    return [Patch(color=color, label=label) for color, label in zip(cmap.colors, vegetation_list)]
 
-# In[3]:
+# Create a cartopy map with Miller projection
+# Set Seaborn theme
+sns.set_theme(style="ticks", font="sans-serif", rc={"lines.linewidth": 2.5})
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 6), sharex=True, sharey=True, subplot_kw={'projection': ccrs.Miller()})
+ax[0].set_extent([-125, -104.5, 31, 48.5])
+ax[1].set_extent([-125, -104.5, 31, 48.5])
 
-
-# Import Current Landcover Data
-historical_model = pd.read_csv("./biogeo/input/current_land_cover.csv",
-    names=["LON", "LAT", "VEGETATION_TYPE"],
-)
-
-
-# Create a dictionary to map the PFT identifiers to the new ones
-mapping_historical = {10: 20, 4: 5, 8: 10, 9: 15, 19: 25, 33: 30}
-historical_model["VEGETATION_TYPE"] = historical_model["VEGETATION_TYPE"].map(
-    mapping_historical
-)
-
-future_model = pd.read_csv("./biogeo/output_bakeoff/end.shift",
-    names=["LON", "LAT", "VEGETATION_TYPE"],
-)
-
-
-# Create a dictionary to map the PFT identifiers to the new ones
-mapping_future = {15: 30, 10: 20, 4: 5, 8: 10, 9: 15, 13: 25, 19: 35, 33: 40}
-future_model ["VEGETATION_TYPE"] = future_model["VEGETATION_TYPE"].map(mapping_future)
-
-
-
-# Create a list of data to plot in each subplot
 data_list = [historical_model, future_model]
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(12, 6), sharex=True, sharey=True)
-
 for i, data in enumerate(data_list):
-    # add missing grids with -99999 value
-    lons = np.arange(-125, -104.5, 0.5)
-    lats = np.arange(31, 49, 0.5)
-    for lon in lons:
-        for lat in lats:
-            if not ((data["LON"] == lon) & (data["LAT"] == lat)).any():
-                #data = data.append({"LON": lon, "LAT": lat, "VEGETATION_TYPE": -99999}, ignore_index=True)
-                data= pd.concat([data, pd.DataFrame({"LON": lon, "LAT": lat, "VEGETATION_TYPE": [-99999],})], ignore_index=True)
-
-    # sort the data by longitude and latitude
-    data = data.sort_values(by=["LON", "LAT"])
-
-    # interpolate data to 0.25 by 0.25
-    lon_new, lat_new = np.meshgrid(np.arange(-125, -104.5, 0.5), np.arange(31, 49, 0.5))
-    z = griddata((data["LON"], data["LAT"]), data["VEGETATION_TYPE"], (lon_new, lat_new), method="linear")
-
-    # plot the data using pcolormesh
-    ax[i].set_title("a) Current dominant PFTs (1984 - 2014)" if i == 0 else "b) Modeled change in dominant PFTs (2070 - 2100)", pad=10)
+    lats = data['LAT'].values
+    lons = data['LON'].values
+    types = data['TYPE'].values
     
-    # Get the appropriate colormap for the subplot
+    # Create a mesh grid
+    unique_lats = np.unique(lats)
+    unique_lons = np.unique(lons)
+    lon, lat = np.meshgrid(unique_lons, unique_lats)
+    grid_types = np.empty(lon.shape, dtype=float)
+
+    for m, ulat in enumerate(unique_lats):
+        for n, ulon in enumerate(unique_lons):
+            mask = (lats == ulat) & (lons == ulon)
+            if mask.any():
+                grid_types[m, n] = types[mask][0]
+            else:
+                grid_types[m, n] = np.nan
+
+    # Use different color mappings for the two subplots
     if i == 0:
-        cmap = current_vegetation_cmap
+        colors_map = {
+            4: "#0F52BA",
+            8: "#9ACD32",
+            9: "#1b9d77",
+            10: "#7570b3",
+            19: "#A4D4B4",
+            33: "#004953",
+        }
     else:
-        cmap = future_vegetation_cmap
-        
-        
-    sns.set_theme(style="ticks", font="sans-serif", rc={"lines.linewidth": 2.5})
-    m = Basemap(
-        projection="mill",
-        llcrnrlat=31,
-        llcrnrlon=-125,
-        urcrnrlat=49,
-        urcrnrlon=-104.5,
-        resolution="l",
-        ax=ax[i]
-    )
-    m.drawcoastlines()
-    m.drawcountries()
-    m.drawstates()
-    x, y = m(lon_new, lat_new)
-
-    # set the color of grids with VEGETATION_TYPE == -99999 to white
-    z_masked = np.ma.masked_where(z == -99999, z)
+        colors_map = {
+            4: "#0F52BA",
+            8: "#9ACD32",
+            9: "#1b9d77",
+            10: "#7570b3",
+            13: "#EEBC1D",
+            15: "#cb7e0c",
+            19: "#A4D4B4",
+            33: "#004953",
+        }
     
-    
-    
+    cmap = ListedColormap(list(colors_map.values()))
+    norm = BoundaryNorm(list(colors_map.keys()), cmap.N)
 
-    m.pcolormesh(x, y, z_masked, cmap=cmap,edgecolor="white", linewidth=0.5)
+    # Use pcolormesh to plot the grid with white borders
+    mesh = ax[i].pcolormesh(lon, lat, grid_types, cmap=cmap, norm=norm, edgecolors='white', linewidth=0.5, transform=ccrs.PlateCarree(), shading='auto')
+    ax[i].add_feature(cfeature.COASTLINE)
+    ax[i].add_feature(cfeature.BORDERS, linestyle='-', alpha=.2)
+    ax[i].add_feature(cfeature.STATES, linestyle='-', alpha=.2)
+    ax[i].set_title("a) Current dominant PFTs (1984 - 2014)" if i == 0 else "b) Modeled change in dominant PFTs (2070 - 2100)", pad=10)
 
-    # set the intervals for parallels and meridians
-    lat_interval = 5
-    lon_interval = 5
-
-    # draw parallels and meridians
-    parallels = np.arange(25, 50, lat_interval)
-    meridians = np.arange(-130, -104.5, lon_interval)
-    m.drawparallels(
-        parallels, labels=[True, False, False, False], fontsize=12, linewidth=0.001
-    )
-    m.drawmeridians(
-        meridians, labels=[False, False, False, True], fontsize=12, linewidth=0.001
-    )
-
-    # Create a list of patches for the legend
-    patches = [
-        Patch(color=color, label=label) for color, label in zip(future_vegetation_cmap.colors, future_vegetation_list)
-    ]
-
- 
-
- # Create a list of patches for the legend
-    patches = [
-        Patch(color=color, label=label) for color, label in zip(future_vegetation_cmap.colors, future_vegetation_list)
-    ]
+# Create patches for the legend
+patches = create_patches(future_vegetation_cmap, future_vegetation_list)
 
 # Add the legend to the figure
 fig.legend(
@@ -182,8 +120,10 @@ fig.legend(
     frameon=False,
 )
 
-plt.savefig("./biogeo/result." + image_format, format=image_format, dpi=1200, bbox_inches="tight")
+
+plt.savefig("./biogeo/result.png", format="png", dpi=1200, bbox_inches="tight")
 plt.show()
+
 
 
 
