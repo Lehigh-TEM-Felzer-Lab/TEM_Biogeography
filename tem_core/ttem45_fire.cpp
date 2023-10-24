f37c9d7645353fb07277b53d533de0face6eb025
Jared M Kodero <47959376+Jared-Kodero@users.noreply.github.com>
2023-10-23 21:13:24 -0400

    Update ttem45_fire.cpp

--- tem_core/ttem45_fire.cpp
+++ tem_core/ttem45_fire.cpp
@@ -2,21 +2,29 @@
 #include "ttem45_fire.h"
 #include <chrono>
 #include <cmath>
-#include <cstdlib> // for exit
-#include <ctime>   // for time (used with srand)
+#include <cstdlib>
+#include <ctime> 
 #include <fstream>
 #include <iostream>
 #include <random>
 #include <sstream>
 #include <tuple>
 #include <unordered_map>
+#include <algorithm>  
+
+
+// Set the seed for the random number generator
+static std::mt19937 generator(std::chrono::system_clock::now().time_since_epoch().count());
+static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+
 
 bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vpr, double vpdn, double vpdd,
-                  int vegtype, double col, double row, int pdm,
+                  int vegtype, double col, double row, int pdm,double ws,
                   // output
                   double &rh, double &theta, double &theta_e, double &fire_probability_threshold,
                   double &fireRandomness, double &severity, double &fRH, double &ftheta, double &Ni, double &fb,
-                  double &fm, double &fs, double &Nf, double &fireProbability, double &dwood, double &dleaf)
+                  double &fm, double &fs, double &Nf, double &fireProbability, double &dwood, double &dleaf,double &Ag, double &Ab)
 {
     // Define constants for the model
     const double UPPER_FUEL_THRESHOLD = 1050.0;    // gCm^-2
@@ -24,22 +32,77 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
     const double UPPER_RH_THRESHOLD = 70.0;        // %
     const double LOWER_RH_THRESHOLD = 30.0;        // %
     const double EPSILON = 1e-6;                   // a small number to avoid division by zero
-    const double SAVERITY_MIN = 0.5;               // minimum fire severity
-    const double SAVERITY_MAX = 1.0;               // maximum fire severity
 
-
-
-    fire_probability_threshold = 0.2;              // minimum fire probability threshold
-    theta_e = 0.69;                                // soil moisture threshold  
-    severity = 0;                                    // fire severity
+    // Fire Variables
+    bool fire = false; // fire occurrence
     rh = vpr / ((vpdn + vpdd) / 2.0 + vpr) * 100.0; // relative humidity
     theta = (sm - wiltpt) / awcapmm;                // soil wetness
+    theta_e = 0.69;                                // soil moisture threshold 
+    fire_probability_threshold = 0.5;              // minimum fire probability threshold
+    severity = 0;                                    // fire severity
+
+
+    // Fire Spread Variables
+    double g0 = 0.05;
+    double beta_low = 0.3;
+    double beta_up = 0.7;
+    double tau = 86400; // 1 day average fire duration 
+
+    // Compute C_beta using linear function
+    double C_beta = beta_low + theta * (beta_up - beta_low);
+    
+    double LB = 1.0 + 10.0 * (1.0 - exp(-0.06 * ws));
+    double HB = LB + (sqrt(pow(LB, 2) - 1)) / (LB - sqrt(pow(LB, 2) - 1));
+
+    // Compute up
+    double gW = (2 * LB / (1 + 1 / HB)) * g0;
+    double umax; // Based on the given vegtype, set the umax value
+    
+    if (vegtype == 4)
+    {
+        umax = 0.11;
+    }
+    else if (vegtype == 8)
+    {
+        umax = 0.11;
+    }
+    else if (vegtype == 9)
+    {
+        umax = 0.15;
+    }
+    else if (vegtype == 10)
+    {
+        umax = 0.11;
+    }
+    else if (vegtype == 13)
+    {
+        umax = 0.2;
+    }
+    else if (vegtype == 15)
+    {
+        umax = 0.17;
+    }
+    else if (vegtype == 19)
+    {
+        umax = 0.11;
+    }
+    else if (vegtype == 33)
+    {
+        umax = 0.11;
+    }
+    else
+    {
+        umax = 0.11;
+    }
+
+    double up = umax * C_beta * rh * gW;
+    Ab =((M_PI* (pow(up,2)) *(pow(tau,2)) )/ (4 * LB)) * pow((1 + 1 / HB), 2) * 1e-6;
+
 
     double lambda = row; // latitude of the grid cell (initializing to row for now)
     double Il = 0.0;     // lightning flashes (per month)
     double Dp = 0.0;     // population density of the grid cell
-    double Ag = 0.0;     // area of the grid cell
-
+  
     struct Data
     {
         double area;
@@ -63,7 +126,7 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
 
     if (!fireData)
     {
-        std::cerr << "Error : File containing area, population density, lightning and flashes not found!" << std::endl;
+        std::cerr << "Error : File containing area, population density and lightning flashes not found!" << std::endl;
         exit(1);
     }
 
@@ -81,12 +144,12 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
     }
     fireData.close();
 
-    bool fire = false; // fire occurrence
+    
 
     auto it = data_map.find(std::make_tuple(col, row, pdm + 1));
     if (it == data_map.end())
     {
-        std::cerr << "Error: No data found for : " << col << " " << row << " " << pdm + 1 << std::endl;
+        std::cerr << "Error: No data found for area, population density and lightning flashes! : " << col << " " << row << " " << pdm + 1 << std::endl;
         exit(1);
     }
     else
@@ -97,8 +160,20 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
         Il = data.flashes;            // lightning flashes (per month) 
         Dp = data.population_density; // population density of the grid cell
         Ag = data.area;               // area of the grid cell
+
+      if (vegtype == 8)
+      {
+          Ag = Ag / 4; // Each PFT is 1/4 of the grid cell
+          Il = Il / 4;
+           Dp = Dp / 4;
+      }
+      else
+        {
+            Ag = Ag / 3; // Each PFT is 1/3 of the grid cell
+            Il = Il/3;
+            Dp = Dp/3;
+        }
      
-        std::cout << "Data found for " << col << " " << row << " " << pdm + 1 << " " << Il << " " << Dp << " " << Ag << std::endl;
         
         
     }
@@ -122,39 +197,35 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
     double epsilon2 = 0.98;
     fs = epsilon1 - epsilon2 * exp(-0.025 * Dp);
 
+ 
+
     // Calculate fuel availability
-    fb = (vegc - LOWER_FUEL_THRESHOLD) / (UPPER_FUEL_THRESHOLD - LOWER_FUEL_THRESHOLD);
-    if (fb < 0)
+  fb = (vegc - LOWER_FUEL_THRESHOLD) / (UPPER_FUEL_THRESHOLD - LOWER_FUEL_THRESHOLD);
+    if (fb < 0.0)
     {
-        fb = 0.0;
-    }
-    else if (fb > 1)
+    fb = 0.0;
+    }   else if (fb > 1.0)
     {
-        fb = 1.0;
+    fb = 1.0;
     }
 
     // Calculate relative humidity factor
-    fRH = (UPPER_RH_THRESHOLD - rh) / (UPPER_RH_THRESHOLD - LOWER_RH_THRESHOLD);
-    if (fRH < 0)
+    fRH = 1.0;
+    if (rh > UPPER_RH_THRESHOLD)
     {
         fRH = 0.0;
     }
-    else if (fRH > 1)
+    else if (rh > LOWER_RH_THRESHOLD)
     {
-        fRH = 1.0;
+        fRH = (UPPER_RH_THRESHOLD - rh) / (UPPER_RH_THRESHOLD - LOWER_RH_THRESHOLD);
     }
 
     // Calculate soil wetness factor
-    if (theta < 0)
-    {
-        theta = 0.0;
-    }
-    else if (theta > 1)
-    {
-        theta = 1.0;
-    }
-
     ftheta = exp(-M_PI * pow(theta / theta_e, 2));
+    if (theta > theta_e)
+    {
+        ftheta = 0.0;
+    }
 
     // Calculate fuel combustibility
     fm = fRH * ftheta;
@@ -162,72 +233,34 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
     // Maximum fire count in a grid cell
     double Nf_max = Ni;
 
-    // Calculate fire occurrence probability
-
-    // Ignition probability
+    // Calculate  Nf (number of fires)
      Nf = Ni * fb * fm * (1.0 - fs);
-    // double fireProbability = Nf / Nf_max;
-    fireProbability = (log(Nf + 1) / log(Nf_max + 1));
+     
 
-    if (fireProbability < 0)
-    {
-        fireProbability = 0.0;
-    }
-    else if (fireProbability > 1)
-    {
-        fireProbability = 1.0;
-    }
+     // adjust area burned based on fire count
+    Ab *= (Nf/Nf_max);
+
+
+    // Calculate fire probability
+    fireProbability = Nf / Nf_max;
+
 
     // Calculate fire severity
-    double dampeningFactor = (1 - (theta / 0.69)) * ((UPPER_RH_THRESHOLD - rh) / UPPER_RH_THRESHOLD);
-    if (dampeningFactor < 0)
-    {
-        dampeningFactor = 0.0;
-    }
-    else if (dampeningFactor > 1)
-    {
-        dampeningFactor = 1.0;
-    }
+    severity =  fb*fm;
 
-    // Determine fire severity based on various conditions
-    if (rh > UPPER_RH_THRESHOLD || vegc < LOWER_FUEL_THRESHOLD)
-    {
-        severity = 0;
-    }
-    else if (vegc > UPPER_FUEL_THRESHOLD)
-    {
-        severity = SAVERITY_MAX * dampeningFactor;
-    }
-    else
-    {
-        severity = (SAVERITY_MIN + (SAVERITY_MAX - SAVERITY_MIN) * (vegc - LOWER_FUEL_THRESHOLD) /
-                                       (UPPER_FUEL_THRESHOLD - LOWER_FUEL_THRESHOLD + EPSILON)) *
-                   dampeningFactor;
-    }
-    if (severity < 0)
-    {
-        severity = 0.0;
-    }
-    else if (severity > 1)
-    {
-        severity = 1.0;
-    }
 
-    // check if fire occurs
 
-    // Create a random engine seeded with the current time
-    // Define a uniform real distribution between 0 and 1
-    std::default_random_engine generator(std::chrono::system_clock::now().time_since_epoch().count());
-    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+// Calculate fire randomness
+
     double randomValue1 = distribution(generator);
     double randomValue2 = distribution(generator);
     double randomValue3 = distribution(generator);
     fireRandomness = (sin(randomValue1 * M_PI) + cos(randomValue2 * M_PI * 2.0) + randomValue3) / 3.0;
 
     // // Bias the fire probability based on factors.
-    double firebiasFactor = 1 - (fb * fm * (1.0 - fs));
+    double environmentFactor = 1 - (fb * fm * (1.0 - fs)); // This is the probability of fire given fuel, moisture, and suppression
 
-    if ((Nf > 0) && (fireRandomness < firebiasFactor))
+    if ((Ab > 0) && (fireRandomness < environmentFactor))
     {
 
         fire = true; // set to true if probability is high enough and random factor is less than probability
@@ -242,8 +275,11 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
 
 
 
-    // Set dwood and dleaf based on vegtype dwood and dleaf are the combustion completeness of wood and leaf, taken
-    // from CLM 5.0
+
+
+
+
+    // Set dwood and dleaf based on vegtype dwood and dleaf are the combustion completeness of wood and leaf, taken from CLM 5.0
     if (vegtype == 4)
     {
         dwood = 0.175;
@@ -284,6 +320,9 @@ bool isFireTrue(double vegc, double sm, double wiltpt, double awcapmm, double vp
         dwood = 0.15;
         dleaf = 0.70;
     }
+
+    dwood *=(Ab/Ag);
+    dleaf *=(Ab/Ag); 
 
     return fire;  // Assuming fire is a boolean variable defined in fire.cpp
 }
